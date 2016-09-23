c8d94c9ffb07cd5ffc2b51cbe2558b397c9f5f4c
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-09-24 00:12:13 +0530

    Add api endpoints to main controller

--- public/js/main.js
+++ public/js/main.js
@@ -12,5 +12,21 @@ angular.module('PassApp', ['ui.router'])
 .controller('MainCtrl', function($scope, ApiService){
 	console.log("In main controller...");
 	var results = { data: "Here comes chaos!" };
-	ApiService.postResults(results);
+	var exam = { data: "Here comes order!" };
+
+	$scope.createResult = function(){
+		ApiService.postResults(results);
+	}
+
+	$scope.createExam = function(){
+		ApiService.createExam(exam);
+	}
+
+	$scope.findResultById = function(){
+		ApiService.getResultById(1, 1, 1).then(function(success){
+			console.log(success);
+		}).catch(function(error){
+			console.log(error);
+		})
+	}
 })
